cdfa9490f4aff64ac2892f1640ba594802cfd29c
Jens <32913789+Jens0512@users.noreply.github.com>
2017-11-14 09:13:42 +0100

    Fix

--- src/main/java/jn/rocbot/Bot.java
+++ src/main/java/jn/rocbot/Bot.java
@@ -54,9 +54,9 @@ public class Bot extends ListenerAdapter {
         }
 
         //Showing masters
-        StringJoiner masters = new StringJoiner("\t\n");
+        StringJoiner masters = new StringJoiner("\n\t");
         for(Masters.Master master : Masters.MASTERS)  masters.add(master.name);
-        Main.log(INFO, "My masters are: " + masters);
+        Main.log(INFO, ("My masters are: " + masters);
 
         //say(event, "I have rebooted");
     }
