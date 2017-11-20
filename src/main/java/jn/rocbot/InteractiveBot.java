a6c04d51e29e022d4f7b52c4de38f0aed95cad2f
jens0512 <Jens0512@osloskolen.no>
2017-11-20 14:07:40 +0100

    Fixed test command

---
+++ src/main/java/jn/rocbot/InteractiveBot.java
@@ -0,0 +1,11 @@
+package jn.rocbot;
+
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class InteractiveBot extends ListenerAdapter{
+    public static void main(String[] args){
+
+    }
+
+
+}
